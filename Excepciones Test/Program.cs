c89147c781b3941d11d62e839803e0ddb829bad8
kevinquemag <kevinquemag2@gmail.com>
2020-12-18 00:01:30 -0500

    version_1

---
+++ Excepciones Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Excepciones_Ej
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            try
+            {
+                Remo barca = new Remo { nombre = "Titanic", Numdekilometrosqueseremo = 0 };
+                barca.Numdekilometrosqueseremo = 5;
+                if (barca.Numdekilometrosqueseremo >= 3)
+                {
+                    throw new Exception("Calambre en los brazos");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("El bote llego al puerto");
+            }
+        }
+
+        public class Remo
+        {
+            public string nombre { get; set; }
+            public int Numdekilometrosqueseremo { get; set; }
+
+        }
+
+    }
+}
+
+
+
+
